eb83eab84499c980b69855c346bb5ce5a8e64bb1
Ishmam Ahmed <ishmam378@gmail.com>
2026-04-19 02:11:48 +0600

    Implement session check and navigation links
    Add session handling and login links to index.php

--- index.php
+++ index.php
@@ -1,37 +1,13 @@
-<?php include("templates/header.php"); ?>
-<?php include("templates/sidebar.php"); ?>
+<?php
+session_start();
 
-<div class="main">
+if(isset($_SESSION['student_id'])){
+    header("Location: dashboard.php");
+    exit();
+}
+?>
 
-<h1>Dashboard</h1>
+<h1>Campus Simulator</h1>
 
-<div class="cards">
-
-<div class="card">
-<h3>Students</h3>
-<p>Manage student profiles</p>
-</div>
-
-<div class="card">
-<h3>Semester Plan</h3>
-<p>Simulate course workload</p>
-</div>
-
-<div class="card">
-<h3>Prediction</h3>
-<p>View GPA predictions</p>
-</div>
-
-<div class="card">
-<h3>Recommendations</h3>
-<p>Improve performance</p>
-</div>
-
-</div>
-
-</div>
-
-</div>
-
-</body>
-</html>
+<a href="login.php">Login</a><br><br>
+<a href="signup.php">Sign Up</a>
